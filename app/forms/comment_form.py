3e7f22a1d1c5dd299e3f9028e4c0905cf8c98af0
Max Koerner <mkoerner570@gmail.com>
2022-08-16 13:18:26 -0400

    comment form under construction

---
+++ app/forms/comment_form.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, Integerfield, SubmitField, FileField, HiddenField
+from wtforms.validators import DataRequired, Email, ValidationError
+from app.models import User
+
+class CommentForm(FlaskForm):
+    post_id=HiddenField("post_id")
+    content=TextAreaField("content")
+    submit=SubmitField("Submit")
+
+class EditCommentForm(FlaskForm):
+    post_id=HiddenField("post_id")
+    content=TextAreaField("content")
+    submit=SubmitField("Submit")
