493f31c247b2433bd109e7f583f7309e3e8886ff
Max Koerner <mkoerner570@gmail.com>
2022-08-29 16:31:40 -0400

    comment model in progress

--- app/models/comments.py
+++ app/models/comments.py
@@ -1,7 +1,16 @@
 from .db import db
 
 class Comments(db.Model):
+    __tablename__ = "comments"
 
-    return{
-        
-    }
+    id = db.Column(db.Interger, primary_key=True)
+    body = db.Column(db.TextArea)
+    pic = db.Column(db.String)
+    likes = db.Column(db.Interger)
+
+    user_id = db.Column(db.Interger, db.ForeignKey("users.id"))
+    user = db.relationship("User", back_populates="comments")
+
+    post_id = db.Column(db.Interger, db.ForeignKey("posts.id"))
+    posts = db.relationship("Posts", back_populates="comments")
+
