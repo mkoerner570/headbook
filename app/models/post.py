94eda5f7c811118baeb805bdd6510975d12cf7f4
Max Koerner <mkoerner570@gmail.com>
2022-08-29 16:14:19 -0400

    posts look complete

--- app/models/post.py
+++ app/models/post.py
@@ -6,3 +6,17 @@ class Posts(db.Model):
     id = db.Column(db.Interger, primary_key=True)
     body = db.Column(db.String)
     pic = db.Column(db.String)
+    likes = db.Column(db.Interger)
+
+    user_id = db.Column(db.Interger, db.ForeignKey('users.id'))
+    users. db.relationship("User", back_populates="posts")
+    comments = db.relationship("Comments", back_populates="posts", cascade="all, delete")
+
+    def to_dict(self):
+        return{
+            "id":self.id,
+            "body":self.body,
+            "pic":self.pic,
+            "likes":self.likes,
+            "user_id":self.user_id,
+        }
