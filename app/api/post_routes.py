bec23f10794547865782ecd6ac414fc60e00801b
Max Koerner <mkoerner570@gmail.com>
2022-08-30 13:34:01 -0400

    delete route constructed

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -9,6 +9,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 
 post_routes = Blueprint('posts', __name__)
 
+# Get all the posts from the database
 @post_routes.route('/posts')
 def all_posts():
     posts = Posts.query.all()
@@ -16,9 +17,15 @@ def all_posts():
 
 
 
-# Get all posts from the database
-@post_routes.route("/posts/<int:id>/one")
+# Get a single post from the database
+@post_routes.route('/posts/<int:id>/one')
 def single_post(id):
     singlePost = Posts.query.filter(Posts.id == id).first()
     return {'singlePost':singlePost.to_dict()}
 
+@post_routes.route('/delete/<int:id>', methods=['DELETE'])
+def delete_post(id):
+    post = Posts.query.get(id)
+    db.session.delete(post)
+    db.session.commit()
+    return post.to_dict()
