2406acafaeb0f5813726ef5009ffa0c2aae8bd5d
Max Koerner <mkoerner570@gmail.com>
2022-08-30 12:53:16 -0400

    vscode syntax error thrown, fixed

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, Jsonify, session, request
 import json
-from app.models import User, Post, db
+from app.models import User, Posts, db
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from app.forms import PostForm
@@ -11,5 +11,14 @@ post_routes = Blueprint('posts', __name__)
 
 @post_routes.route('/posts')
 def all_posts():
-    posts = Post.query.all()
-    return {'post': [post.to_dict() for post in posts]}
+    posts = Posts.query.all()
+    return {'post': [post.to_dict() for post in posts ]}
+
+
+
+# Get all posts from the database
+@post_routes.route("/posts/<int:id>/one")
+def single_post(id):
+    singlePost = Posts.query.filter(Posts.id == id).first()
+    return {'singlePost':singlePost.to_dict()}
+
