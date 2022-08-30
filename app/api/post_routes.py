ff244b9123455cec1e614b91b7f5805558b7933d
Max Koerner <mkoerner570@gmail.com>
2022-08-30 12:47:55 -0400

    all posts api route made

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -7,3 +7,9 @@ from app.forms import PostForm
 from app.forms import EditPostForm
 from flask_login import current_user, login_user, logout_user, login_required
 
+post_routes = Blueprint('posts', __name__)
+
+@post_routes.route('/posts')
+def all_posts():
+    posts = Post.query.all()
+    return {'post': [post.to_dict() for post in posts]}
