74a8165eb3950717dbafe0f94c8642ce7d759028
Max Koerner <mkoerner570@gmail.com>
2022-09-01 13:00:55 -0400

    get all comments constructed

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -7,3 +7,9 @@ from flask_login import current_user, login_user, logot_user,login_required
 
 comment_routes = Blueprint('comments', __name__)
 
+# Get all Comments from the database
+@comment_routes.route('/posts/<int:id>/comments')
+def get_comments(id):
+    post_comments = Comments.query.filter(Comments.post_id == id).all()
+    return {'allComments':[comment.to_dict() for comment in post_comments]}
+
